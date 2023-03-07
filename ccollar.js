bb83348f713a826f5aac9a788c24378f9e1f3097
littlesera <126942824+littlesera@users.noreply.github.com>
2023-03-06 22:32:11 -0800

    Update ccollar.js

--- ccollar.js
+++ ccollar.js
@@ -38,7 +38,7 @@ var bcModSDK=function(){"use strict";const e="1.1.0";function o(e){alert("Mod ER
     
     const w = window;
 
-    //await bcarSettingsLoad();
+    //await bcarSettingsLoad(); 
 
 
     if(Player.CCollar != null){
