6ce2520c1a4496e00a40a1c44927a04d28de74f4
Sera <tehkyle+sera@gmail.com>
2023-03-17 08:46:44 -0700

    add kneel on "sit in lap"

--- dist/bundle.js
+++ dist/bundle.js
@@ -1639,3 +1639,13 @@ function BigProtestBoopReact(booper) {
     boopShutdown = true;
     setTimeout(() => boopShutdown = false, 30000);
 }
+
+OnActivity(100, "Little Sera Boops", (data, msg, sender, metadata) => {
+    let target = data.Dictionary.find(d => d.Tag == "TargetCharacter");
+    if (!!target && 
+        sender.MemberNumber == Player.MemberNumber && 
+        data.Content == "ChatOther-ItemLegs-Sit" &&
+        CharacterCanChangeToPose(Player, "Kneel")) {
+        CharacterSetActivePose(Player, "Kneel");
+    }
+});
