356bd3c7c9ddd8161d157be295380a46db0df352
Little Sera <tehkyle+sera@gmail.com>
2023-04-21 12:50:41 -0700

    Make collar commands customizable

--- src/Settings/collar.ts
+++ src/Settings/collar.ts
@@ -17,8 +17,10 @@ export class GuiCollar extends GuiSubscreen {
 		if (Player.LSCG.CollarModule === undefined) {
 			Player.LSCG.CollarModule = <CollarSettingsModel>{ 
 				enabled: false,
-				allowedMembers: Player.Ownership?.MemberNumber+"" ?? "",
-				chokeLevel: 0
+				allowedMembers: Player.Ownership?.MemberNumber + "" ?? "",
+				chokeLevel: 0,
+				tightTrigger: "tight",
+				looseTrigger: "loose"
 			};
 		}
 		return Player.LSCG.CollarModule;		
@@ -27,6 +29,8 @@ export class GuiCollar extends GuiSubscreen {
 	Load(): void {
 		super.Load();
 		ElementCreateInput("collar_allowedMembers", "text", this.settings.allowedMembers ?? (Player.Ownership?.MemberNumber+"" ?? ""), "255");
+		ElementCreateInput("collar_tightTrigger", "text", this.settings.tightTrigger ?? "tight", "255");
+		ElementCreateInput("collar_looseTrigger", "text", this.settings.looseTrigger ?? "loose", "255");
 	}
 
     Run() {
@@ -49,12 +53,24 @@ export class GuiCollar extends GuiSubscreen {
 		MainCanvas.textAlign = "center";
 		DrawButton(GuiSubscreen.START_X + 600, this.getYPos(3) - 32, 200, 64, "Update", "White", undefined, "Update Collar to Current", !this.settings.enabled);
 
+		// Tighten Trigger 			[string]
+		DrawText("Tighten Trigger:", GuiSubscreen.START_X, this.getYPos(4), "Black", "Gray");
+		ElementPosition("collar_tightTrigger", GuiSubscreen.START_X + 900, this.getYPos(4), 600);
+
+		// Loosen Trigger 			[string]
+		DrawText("Loosen Trigger:", GuiSubscreen.START_X, this.getYPos(5), "Black", "Gray");
+		ElementPosition("collar_looseTrigger", GuiSubscreen.START_X + 900, this.getYPos(5), 600);
+
 		MainCanvas.textAlign = prev;
 	}
 
 	Exit(): void {
 		this.settings.allowedMembers = ElementValue("collar_allowedMembers") ?? (Player.Ownership?.MemberNumber+"" ?? "");
+		this.settings.tightTrigger = ElementValue("collar_tightTrigger") ?? "tight";
+		this.settings.looseTrigger = ElementValue("collar_looseTrigger") ?? "loose";
 		ElementRemove("collar_allowedMembers");
+		ElementRemove("collar_tightTrigger");
+		ElementRemove("collar_looseTrigger");
 
 		if (!this.settings.chokeLevel)
 			this.settings.chokeLevel = 0;
