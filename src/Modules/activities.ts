d1c4aa6f4afcdd6f14fedea21afd1a1f781c0171
Little Sera <littleserabc@gmail.com>
2023-04-26 14:41:53 -0700

    Add more activities. tail wag, nibble, and riding. Still need to work out arousal..

--- src/Modules/activities.ts
+++ src/Modules/activities.ts
@@ -6,16 +6,23 @@ import { hypnoActivated } from "./hypno";
 
 export interface ActivityTarget {
     Name: AssetGroupItemName;
-    SelfAllowed?: boolean | true;
+    SelfAllowed?: boolean | false;
+    SelfOnly?: boolean | false;
     TargetLabel?: string | undefined;
     TargetSelfLabel?: string | undefined;
     TargetAction: string;
     TargetSelfAction?: string | undefined;
 }
 
+export interface CustomPrerequisite {
+    Name: string;
+    Func(acting: Character, acted: Character, group: AssetGroup): boolean;
+}
+
 export interface ActivityBundle {
     Activity: Activity;
     Targets: ActivityTarget[];
+    CustomPrereqs?: CustomPrerequisite[];
 }
 
 export class ActivityModule extends BaseModule {
@@ -35,7 +42,6 @@ export class ActivityModule extends BaseModule {
                         Text: msg
                     });
                     return next(args);
-                    //SendAction(msg, Player);
                 }
                 else
                     return next(args);
@@ -45,10 +51,29 @@ export class ActivityModule extends BaseModule {
             }
         }, ModuleCategory.Activities);
 
+        hookFunction("ActivityCheckPrerequisite", 100, (args, next) => {
+            var prereqName = <string>args[0];
+            if (this.CustomPrerequisiteFuncs.has(prereqName)) {
+                var acting = args[1];
+                var acted = args[2];
+                var targetGrp = args[3];
+                var customPrereqFunc = this.CustomPrerequisiteFuncs.get(prereqName);
+                if (!customPrereqFunc)
+                    return next(args);
+                else {
+                    return customPrereqFunc(acting, acted, targetGrp);
+                }
+            }
+            else
+                return next(args);
+        })
+
+        // Hug
         this.AddActivity({
             Activity: <Activity>{
                 Name: "Hug",
                 MaxProgress: 70,
+                MaxProgressSelf: 70,
                 Prerequisite: ["UseArms"]
             },
             Targets: [
@@ -61,10 +86,12 @@ export class ActivityModule extends BaseModule {
             ]
         });
 
+        // Tackle
         this.AddActivity({
             Activity: <Activity>{
                 Name: "Tackle",
                 MaxProgress: 50,
+                MaxProgressSelf: 50,
                 Prerequisite: ["UseArms"]
             },
             Targets: [
@@ -76,10 +103,12 @@ export class ActivityModule extends BaseModule {
             ]
         });
 
+        // Flop
         this.AddActivity({
             Activity: <Activity>{
                 Name: "Flop",
                 MaxProgress: 50,
+                MaxProgressSelf: 50,
                 Prerequisite: ["UseLegs"]
             },
             Targets: [
@@ -91,10 +120,12 @@ export class ActivityModule extends BaseModule {
             ]
         });
 
+        // KissEyes
         this.AddActivity({
             Activity: <Activity>{
                 Name: "KissEyes",
                 MaxProgress: 75,
+                MaxProgressSelf: 50,
                 Prerequisite: ["ZoneAccessible"]
             },
             Targets: [
@@ -107,10 +138,30 @@ export class ActivityModule extends BaseModule {
             ]
         });
 
+        // RubPussy
+        this.AddActivity({
+            Activity: <Activity>{
+                Name: "RubPussy",
+                MaxProgress: 100,
+                MaxProgressSelf: 100,
+                Prerequisite: ["ZoneAccessible", "ZoneNaked", "HasVagina"]
+            },
+            Targets: [
+                <ActivityTarget>{
+                    Name: "ItemPenis",
+                    SelfAllowed: false,
+                    TargetLabel: "Rub Pussy",
+                    TargetAction: "SourceCharacter grinds their pussy against TargetCharacter's penis."
+                }
+            ]
+        });
+
+        // RubPenis
         this.AddActivity({
             Activity: <Activity> {
                 Name: "RubPenis",
-                MaxProgress: 95,
+                MaxProgress: 100,
+                MaxProgressSelf: 100,
                 Prerequisite: ["ZoneAccessible", "ZoneNaked", "CanUsePenis", "HasPenis", "Needs-PenetrateItem"]
             },
             Targets: [
@@ -164,12 +215,114 @@ export class ActivityModule extends BaseModule {
                     TargetAction: "SourceCharacter runs PronounPossessive penis against TargetCharacter's own penis."
                 }
             ]
-        })
+        });
+
+        // WagTail
+        this.AddActivity({
+            Activity: {
+                Name: "WagTail",
+                MaxProgress: 50,
+                MaxProgressSelf: 50,
+                Prerequisite: [],
+                Target: []
+            },
+            Targets: [{
+                Name: "ItemButt",
+                SelfAllowed: true,
+                SelfOnly: true,
+                TargetLabel: "Wag Tail",
+                TargetAction: "SourceCharacter wags PronounPossessive tail."
+            }],
+            CustomPrereqs: [
+                {
+                    Name: "HasTail",
+                    Func: (acting, acted, group) => !!InventoryGet(acted, "TailStraps")
+                }
+            ]
+        });
+
+        // NibbleTail
+        this.AddActivity({
+            Activity: {
+                Name: "Nibble",
+                MaxProgress: 90,
+                MaxProgressSelf: 50,
+                Prerequisite: ["HasTail"],
+                Target: []
+            },
+            Targets: [{
+                Name: "ItemButt",
+                SelfAllowed: true,
+                TargetLabel: "Nibble Tail",
+                TargetAction: "SourceCharacter nibbles on TargetCharacter's tail.",
+                TargetSelfAction: "SourceCharacter nibbles on PronounPossessive own tail."
+            }]
+        });
+
+        // FuckWithPussy
+        this.AddActivity({
+            Activity: <Activity>{
+                Name: "FuckWithPussy",
+                MaxProgress: 100,
+                MaxProgressSelf: 100,
+                Prerequisite: ["ZoneAccessible", "ZoneNaked", "HasVagina", "TargetHasPenis"]
+            },
+            Targets: [
+                {
+                    Name: "ItemVulva",
+                    SelfAllowed: false,
+                    TargetLabel: "Grind with Pussy",
+                    TargetAction: "SourceCharacter grinds PronounPossessive pussy against TargetCharacter's."
+                }, {
+                    Name: "ItemPenis",
+                    SelfAllowed: false,
+                    TargetLabel: "Ride with Pussy",
+                    TargetAction: "SourceCharacter fucks TargetCharacter's penis with PronounPossessive pussy, grinding up and down."
+                }
+            ],
+            CustomPrereqs: [
+                {
+                    Name: "SourceVulvaEmpty",
+                    Func: (acting, acted, group) => !acting.IsVulvaFull()
+                }
+            ]
+        });
+
+        // FuckWithAss
+        this.AddActivity({
+            Activity: <Activity>{
+                Name: "FuckWithAss",
+                MaxProgress: 100,
+                MaxProgressSelf: 100,
+                Prerequisite: ["ZoneAccessible", "ZoneNaked", "TargetHasPenis"]
+            },
+            Targets: [
+                {
+                    Name: "ItemVulva",
+                    SelfAllowed: false,
+                    TargetLabel: "Grind with Ass",
+                    TargetAction: "SourceCharacter grinds PronounPossessive ass against TargetCharacter's vulva."
+                },{
+                    Name: "ItemPenis",
+                    SelfAllowed: false,
+                    TargetLabel: "Ride with Ass",
+                    TargetAction: "SourceCharacter fucks TargetCharacter's penis with PronounPossessive ass."
+                }
+            ],
+            CustomPrereqs: [
+                {
+                    Name: "SourceAssEmpty",
+                    Func: (acting, acted, group) => !acting.IsPlugged()
+                }
+            ]
+        });
     }
 
     unload(): void {
         removeAllHooksByModule(ModuleCategory.Activities);
     }
+
+    CustomPrerequisiteFuncs: Map<string, (acting: Character, acted: Character, group: AssetGroup) => boolean> = new Map<string, (acting: Character, acted: Character, group: AssetGroup) => boolean>();
 
     AddActivity(bundle: ActivityBundle) {
         if (bundle.Targets.length <= 0)
@@ -180,6 +333,12 @@ export class ActivityModule extends BaseModule {
         activity.Prerequisite = activity.Prerequisite ?? [];
         activity.Name = "LSCG_" + activity.Name;
 
+        bundle.CustomPrereqs?.forEach(prereq => {
+            activity.Prerequisite.push(prereq.Name);
+            if (!this.CustomPrerequisiteFuncs.get(prereq.Name))
+                this.CustomPrerequisiteFuncs.set(prereq.Name, prereq.Func)
+        })
+
         ActivityDictionary.push([
             "Activity"+activity.Name,
             bundle.Targets[0].TargetLabel ?? activity.Name.substring(5)
@@ -188,14 +347,22 @@ export class ActivityModule extends BaseModule {
         bundle.Targets.forEach(tgt => {
             tgt.TargetLabel = tgt.TargetLabel ?? activity.Name.substring(5);
 
-            if (activity.Target.indexOf(tgt.Name) == -1) {
-                activity.Target.push(tgt.Name);
+            if (tgt.SelfAllowed) {
+                if (!activity.TargetSelf)
+                    activity.TargetSelf = [];
+                if ((<AssetGroupItemName[]>activity.TargetSelf).indexOf(tgt.Name) == -1) {
+                    (<AssetGroupItemName[]>activity.TargetSelf).push(tgt.Name);
+                }
             }
-            
-            if (!tgt.SelfAllowed)
-                activity.TargetSelf = [];
-            else
-                activity.TargetSelf = true;
+
+            if (!tgt.SelfOnly) {
+                if (!activity.Target)
+                    activity.Target = [];
+
+                if (activity.Target.indexOf(tgt.Name) == -1) {
+                    activity.Target.push(tgt.Name);
+                }            
+            }
 
             ActivityDictionary.push([
                 "Label-ChatOther-" + tgt.Name + "-" + activity.Name,
