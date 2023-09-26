ed690d51492908a3ef9f9aa2552c92c7300d2515
Little Sera <126942824+littlesera@users.noreply.github.com>
2023-09-26 13:40:13 -0700

    add dev tampermonkey loader

---
+++ lscgLoader.user.dev.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name LSCG
+// @namespace https://www.bondageprojects.com/
+// @version 0.3.0
+// @description Little Sera's Club Games
+// @author Little Sera
+// @match https://bondageprojects.elementfx.com/*
+// @match https://www.bondageprojects.elementfx.com/*
+// @match https://bondage-europe.com/*
+// @match https://www.bondage-europe.com/*
+// @run-at document-end
+// @grant none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var script = document.createElement("script");
+    script.type = "module";
+    script.setAttribute("crossorigin", "anonymous");
+    script.src = `https://littlesera.github.io/LSCG/dev/bundle.js?${Date.now()}`;
+    document.head.appendChild(script);
+})();
