bbee434a5aa07e514538aec64ecf55395e3b4372
Little Sera <126942824+littlesera@users.noreply.github.com>
2023-09-29 11:13:25 -0700

    clean up comments

--- src/Modules/States/RedressedState.ts
+++ src/Modules/States/RedressedState.ts
@@ -103,10 +103,6 @@ export class RedressedState extends BaseState {
                 if (isRestore || !(groupBlocked || isBlocked || isRoomDisallowed)) {
                     let newItem = InventoryWear(Player, item.Name, item.Group, item.Color, item.Difficulty, -1, item.Craft, false);
                     if (!!newItem) {
-                        // if (!!item.Property?.LockedBy && InventoryDoesItemAllowLock(newItem)) {
-                        //     let lock = AssetGet(Player.AssetFamily, "ItemMisc", item.Property.LockedBy);
-                        //     if (!!lock) InventoryLock(Player, newItem, {Asset:lock}, item.Property.LockMemberNumber)
-                        // }
                         if (!!item.Property)
                             newItem.Property = item.Property;
                     }
