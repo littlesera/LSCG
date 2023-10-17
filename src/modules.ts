8a81bc8fa1589ca0c475e56f01a108d6607b10c4
Little Sera <126942824+littlesera@users.noreply.github.com>
2023-10-17 10:54:21 -0700

    Add drug keywords, craftable spell names, hypno triggers, and configured activities to first pass

--- src/modules.ts
+++ src/modules.ts
@@ -1,3 +1,8 @@
+import { ActivityModule } from "Modules/activities";
+import { HypnoModule } from "Modules/hypno";
+import { InjectorModule } from "Modules/injector";
+import { MagicModule } from "Modules/magic";
+import { ActivityEntryModel } from "Settings/Models/activities";
 import { BaseModule } from "base";
 
 export const modulesMap: Map<string, BaseModule> = new Map<string, BaseModule>();
@@ -13,4 +18,27 @@ export function registerModule<T extends BaseModule>(module: T): T {
 
 export function getModule<T extends BaseModule>(moduleType: string): T {
 	return modulesMap.get(moduleType) as T;
+}
+
+export function DrugKeywords(): string[] {
+	let settings = getModule<InjectorModule>("InjectorModule")?.settings;
+	if (!settings)
+		return [];
+	return settings?.hornyKeywords.concat(settings?.sedativeKeywords).concat(settings?.mindControlKeywords).concat(settings?.cureKeywords);
+}
+
+export function NetgunKeywords(): string[] {
+	return getModule<InjectorModule>("InjectorModule")?.settings.netgunKeywords;
+}
+
+export function CraftableItemSpellNames(): string[] {
+	return getModule<MagicModule>("MagicModule")?.settings.knownSpells.filter(s => s.AllowPotion).map(s => s.Name) ?? [];
+}
+
+export function HypnoTriggers(): string[] {
+	return getModule<HypnoModule>("HypnoModule")?.triggers;
+}
+
+export function ConfiguredActivities(): ActivityEntryModel[] {
+	return getModule<ActivityModule>("ActivityModule")?.settings.activities;
 }
