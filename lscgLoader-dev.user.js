888d5e6108e987748c43b87fde1b03b86953fb10
Little Sera <126942824+littlesera@users.noreply.github.com>
2023-11-22 10:36:32 -0800

    version bump

--- lscgLoader-dev.user.js
+++ lscgLoader-dev.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name LSCG DEV
 // @namespace https://www.bondageprojects.com/
-// @version 0.3.22
+// @version 0.3.23
 // @description Little Sera's Club Games
 // @author Little Sera
 // @match https://bondageprojects.elementfx.com/*
