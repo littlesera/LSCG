f6d384502234013a10507bb419ed66cf75d33d0f
Little Sera <126942824+littlesera@users.noreply.github.com>
2025-03-18 23:28:05 -0700

    fix suggestion require hypnotizer unchecked

--- src/Settings/Remote/suggestions.ts
+++ src/Settings/Remote/suggestions.ts
@@ -46,7 +46,7 @@ export class RemoteSuggestions extends RemoteHypno {
 			memberIdIsAllowed = this.overrideMemberIds.indexOf(Player.MemberNumber!) > -1;
 
 		var passTranceReq = this.Character.LSCG.StateModule.states.find(s => s.type == "hypnotized")?.active ?? false;
-		var passHypnotizerReq = (this.settings.suggestionRequireHypnotizer && this.Character.LSCG.StateModule.states.find(s => s.type == "hypnotized")?.activatedBy == Player.MemberNumber)
+		var passHypnotizerReq = !this.settings.suggestionRequireHypnotizer || this.Character.LSCG.StateModule.states.find(s => s.type == "hypnotized")?.activatedBy == Player.MemberNumber;
 
 		let reason;
 		if (!memberIdIsAllowed)
