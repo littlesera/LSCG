faef2ecfb434a657cffcdb909b51cbcba34aee18
bananarama92 <bananarama921@outlook.com>
2025-04-09 12:14:57 +0200

    MAINT: Use more standard `C.IsPlayer()` checks for character comparisons

--- src/Modules/States/FrozenState.ts
+++ src/Modules/States/FrozenState.ts
@@ -20,7 +20,7 @@ export class FrozenState extends BaseState {
         this.Restrictions.Wardrobe = "true";
         this.Restrictions.Walk = "true";
         this.Restrictions.Speech = "true";
-        if (GameVersion !== "R114" && (DialogSelfMenuSelected as unknown) === "Expression" && DialogSelfMenuMapping.Expression.C === Player) {
+        if (GameVersion !== "R114" && (DialogSelfMenuSelected as unknown) === "Expression" && DialogSelfMenuMapping.Expression.C.IsPlayer()) {
             // Refresh the expression pannel upon modifying the `Emoticon`, `Walk` or `Eyes` restrictions
             DialogSelfMenuMapping.Expression.Reload();
         }
