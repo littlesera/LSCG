51989f48893ccf1ba9766691be6c33f8bbaaf7fc
bananarama92 <bananarama921@outlook.com>
2026-02-21 00:25:39 +0100

    MAINT: Adapt to the R125 crafting effects changes

--- src/club_existing/declarations.d.ts
+++ src/club_existing/declarations.d.ts
@@ -4,3 +4,9 @@ interface Window {
 	LSCG_Loaded?: boolean;
 	LSCG_Version?: string;
 }
+
+// TODO: Remove once >= R125 types are installed
+interface CraftingItem {
+	/** The crafted item effects mapped to their effect strength. */
+	Effects: Partial<Record<CraftingPropertyType, number>>;
+}
