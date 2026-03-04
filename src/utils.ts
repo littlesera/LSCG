110244929305bc4b6bcc0d48c5b9ba60ec0ebccf
Little Sera <126942824+littlesera@users.noreply.github.com>
2026-03-03 17:58:07 -0800

    prevent pass by reference with arousal settings when copying char

--- src/utils.ts
+++ src/utils.ts
@@ -1232,8 +1232,8 @@ export function CopyCharacter(C: Character, id: string, strip: boolean = true, r
 	let newCharacter = CharacterLoadSimple(`LSCG-${id || C.ID}`);
 	newCharacter.Name = C.Name;
 	newCharacter.Nickname = C.Nickname;
-	newCharacter.ArousalSettings = C.ArousalSettings;
-	
+	newCharacter.ArousalSettings = JSON.parse(JSON.stringify(C.ArousalSettings));
+
 	newCharacter.Appearance = AppearanceItemParse(CharacterAppearanceStringify(C));
 	if (strip) StripCharacterNoRedraw(newCharacter);
 	if (removeItems) CharacterReleaseTotal(newCharacter, false);
